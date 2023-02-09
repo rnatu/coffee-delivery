0fc42b49482987b6b6f0c48c21baf8a132b692a0
Renato Xavier <rnatu91@gmail.com>
2023-02-09 17:12:07 -0300

    feat: add defaultTheme collors

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,22 @@
 export const defaultTheme = {
-  white: '#FFF',
+  white: '#FFFFFF',
+  background: '#FAFAFA',
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  'base-card': '#F3F2F2',
+  'base-input': '#EDEDED',
+  'base-button': '#E6E5E5',
+  'base-hover': '#D7D5D5',
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  'base-label': '#8D8686',
+  'base-text': '#574F4D',
+  'base-subtitle': '#403937',
+  'base-title': '#272221',
 
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
+  'purple-light': '#EBE5F9',
+  purple: '#8047F8',
+  'purple-dark': '#4B2995',
 
-  'yellow-500': '#FBA94C',
+  'yellow-light': '#F1E9C9',
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
 };

0b3de653c1e2da4a3f7753d3b94882d9192e6354
Renato Xavier <rnatu91@gmail.com>
2023-02-10 19:17:34 -0300

    feat: add global and theme for styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import { Router } from './router';
+import { GlobalStyle } from './styles/global';
+import { defaultTheme } from './styles/themes/default';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Router />
-    </BrowserRouter>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <GlobalStyle />
+
+        <Router />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 }
 

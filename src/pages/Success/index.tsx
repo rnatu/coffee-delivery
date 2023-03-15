633dd43b95820c69069829f255adb1852d672268
Renato Xavier <rnatu91@gmail.com>
2023-03-15 20:33:43 -0300

    feat: add InfoWithIcon on Success page

--- src/pages/Success/index.tsx
+++ src/pages/Success/index.tsx
@@ -3,7 +3,7 @@ import { OrderInfo, SuccessContainer } from './styles';
 import successIllustration from '../../assets/success-illustration.svg';
 import { InfoWithIcon } from '../../components/InfoWithIcon';
 import { useTheme } from 'styled-components';
-import { MapPin } from 'phosphor-react';
+import { CurrencyDollar, MapPin, Timer } from 'phosphor-react';
 export function Success() {
   const { colors } = useTheme();
 
@@ -17,10 +17,25 @@ export function Success() {
       <div>
         <OrderInfo>
           <InfoWithIcon backgroundColor={colors['brand-purple']} icon={MapPin}>
-            <p>
+            <RegularText>
               Entrega em <strong>Rua João Daniel Martinelli, 102</strong>{' '}
               Farrapos - Porto Alegre, RS
-            </p>
+            </RegularText>
+          </InfoWithIcon>
+
+          <InfoWithIcon backgroundColor={colors['brand-yellow']} icon={Timer}>
+            <RegularText>
+              Previsão de entrega <strong>20 min - 30 min</strong>
+            </RegularText>
+          </InfoWithIcon>
+
+          <InfoWithIcon
+            backgroundColor={colors['brand-yellow-dark']}
+            icon={CurrencyDollar}
+          >
+            <RegularText>
+              Pagamento na entrega <strong>Cartão de Crédito</strong>
+            </RegularText>
           </InfoWithIcon>
         </OrderInfo>
 
